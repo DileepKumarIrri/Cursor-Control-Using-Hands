9a0e040e7abbab7e038162b73405a55b8b8d796f
Dileep <dil@gmail.com>
2024-09-07 23:33:34 +0530

    Hard Ware Added

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import cv2
 import mediapipe as mp
 import pyautogui
+import serial
+import time
+import math
+
+ser = serial.Serial('COM3', 9600)
 
 hand_detector=mp.solutions.hands.Hands()
 drawing_utils = mp.solutions.drawing_utils
@@ -8,7 +13,7 @@ screen_w, screen_h= pyautogui.size()
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 class vm():    
     def __init__(self):
-        self.video=cv2.VideoCapture(0)
+        self.video=cv2.VideoCapture(1)
     def __del__(self):
         self.video.release()
     def virtual_mouse(self):       
@@ -20,42 +25,33 @@ class vm():
         output = hand_detector.process(rgb_frame)
         hands = output.multi_hand_landmarks
         index_y = 0
+
+        # Initialize click count
+        click_count = 0
+
         if hands:
-            for hand in hands:               
-                drawing_utils.draw_landmarks(frame, hand)
-                landmarks = hand.landmark
-                for id, landmark in enumerate(landmarks):
-                    x = int(landmark.x*frame_w)
-                    y = int(landmark.y*frame_h)
-                    print(x, y)
-                    if id == 8:
-                        cv2.circle(img=frame, center=(x,y), radius=10, color=(0,255,255))                      
-                        index_x = screen_w/frame_w*x
-                        index_y = screen_h/frame_h*y
-                        pyautogui.moveTo(index_x,index_y)
-                    if id == 4:
-                        cv2.circle(img=frame, center=(x,y), radius=10, color=(0,255,255))
-                        thumb_x = screen_w/frame_w*x
-                        thumb_y = screen_h/frame_h*y
-                        print('outside ',abs(index_y - thumb_y))
-                        if abs(index_y - thumb_y)<20:
-                            pyautogui.click(clicks=2)
-                            pyautogui.sleep(1)
-                    if id == 12:
-                        cv2.circle(img=frame, center=(x,y), radius=10, color=(0,255,255))
-                        mid_x = screen_w/frame_w*x
-                        mid_y = screen_h/frame_h*y
-                        print('outside ',abs(index_y - mid_y))
-                        if abs(index_y - mid_y)<20:
-                            pyautogui.click(clicks=2)
-                            pyautogui.sleep(1)
-                        
+                try: 
+                                data_to_send = '0'  
+                                ser.write(data_to_send.encode())
+                except KeyboardInterrupt:
+                            print("Exiting...")
+                            ser.close()
+            
+                       
+        else:
+                try: 
+                                data_to_send = '1'  
+                                ser.write(data_to_send.encode())
+                except KeyboardInterrupt:
+                            print("Exiting...")
+                            ser.close()
+                
+
         _,jpg=cv2.imencode('.jpg',frame)
         return jpg.tobytes()
-
 class vm2():
     def __init__(self):
-        self.video=cv2.VideoCapture(0)
+        self.video=cv2.VideoCapture(1)
     def __del__(self):
         self.video.release()
     def virtual_mouse2(self):       
@@ -74,17 +70,32 @@ class vm2():
                 if id == 1:
                     screen_x = screen_w * landmark.x
                     screen_y = screen_h * landmark.y
-                    pyautogui.moveTo(screen_x, screen_y)
+                    # pyautogui.moveTo(screen_x, screen_y)
             left = [landmarks[145], landmarks[159]]
             for landmark in left:
                 x = int(landmark.x * frame_w2)
                 y = int(landmark.y * frame_h2)
                 cv2.circle(frame2, (x, y), 3, (0, 255, 255))
-            if (left[0].y - left[1].y) < 0.004:
-                pyautogui.click(clicks=2)
-                pyautogui.sleep(1)
+                print(left[0].y - left[1].y)
+            if (left[0].y - left[1].y) < 0.010:
+                # pyautogui.click(clicks=2)
+                try: 
+                        data_to_send = '0'  
+                        ser.write(data_to_send.encode())
+                except KeyboardInterrupt:
+                    print("Exiting...")
+                    ser.close()
+            # else: 
+            #     try: 
+            #             data_to_send = '1'  
+            #             ser.write(data_to_send.encode())
+            #     except KeyboardInterrupt:
+            #             print("Exiting...")
+            #             ser.close()
+
         _,jpg2=cv2.imencode('.jpg',frame2)
         return jpg2.tobytes()
+    
     
 
 
